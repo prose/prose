f24af3559d04f96231b661abd763316aec3f8e6c
Michael Aufreiter <ma@zive.at>
2012-03-13 01:18:58 -0400

    Load Jekyll config file, which holds Columnist configuration as well.

--- _includes/routers/application.js
+++ _includes/routers/application.js
@@ -8,14 +8,15 @@ routers.Application = Backbone.Router.extend({
     
     // Using this.route, because order matters
     this.route(/(.*\/.*)/, 'post', this.post);
-    this.route(":repo/posts", "posts", app.posts);
-    this.route(":repo/new", "new", app.newPost);
+    this.route(":repo/posts", "posts", app.instance.posts);
+    this.route(":repo/new", "new", app.instance.newPost);
+    this.route("", "start", app.instance.start);
   },
   post: function(url) {
     url = url.split('/');
     var repo = url.slice(0, 1).join('/');
     var path = (url.slice(1) || []).join('/');
-    app.post(repo, path);
+    app.instance.post(repo, path);
   }
 });
 
