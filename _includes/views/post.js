7611a13c83cec54e4b4acd52ac6ed386ab174ad7
tristen <tristen@tristen.ca>
2012-03-14 17:26:19 -0400

    big theme commit and BREAKAGE

--- _includes/views/post.js
+++ _includes/views/post.js
@@ -1,54 +1,54 @@
 (function(config, models, views, routers, utils, templates) {
 
 views.Post = Backbone.View.extend({
-  id: 'post',
-
-  events: {
-    "click .save-post": "save"
-  },
-
-  save: function() {
-    var metadata = {
-      title: this.$('#post_title').val(),
-      subtitle: this.$('#post_subtitle').val(),
-      layout: this.$('#post_layout').val(),
-      category: this.$('#post_category').val(),
-      permalink: this.$('#post_permalink').val(),
-      image: this.$('#post_image').val(),
-      hidden: !this.$('#post_published').val()
+    id: 'post',
+    events: {
+        'click .save-post': 'save',
+        'click .state a': 'publish',
+        'click .meta': 'toggleMeta'
+    },
+    publish: function(e) {
+        e.preventDefault();
+        // TODO replace save with publish and pass in commit message options
+    },
+    save: function() {
+        var metadata = {
+            title: this.$('#post_title').val(),
+            subtitle: this.$('#post_subtitle').val(),
+            layout: this.$('#post_layout').val(),
+            category: this.$('#post_category').val(),
+            permalink: this.$('#post_permalink').val(),
+            image: this.$('#post_image').val(),
+            hidden: !this.$('#post_published').val()
+        }
+        savePost(this.model.repo, this.model.path, metadata, this.editor.getValue(), function(err) {
+            console.log('Saved');
+        });
+        return false;
+    },
+    initialize: function(options) {
+    },
+    toggleMeta: function(e) {
+        e.preventDefault();
+        $('.metadata').toggleClass('open');
+    },
+    initEditor: function() {
+        var that = this;
+        setTimeout(function() {
+            that.editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+                mode: 'markdown',
+                lineWrapping: true,
+                matchBrackets: true,
+                theme: 'default'
+            });
+        }, 200);
+    },
+    render: function() {
+        var that = this;
+        $(this.el).html(templates.post(this.model));
+        this.initEditor();
+        return this;
     }
-
-    savePost(this.model.repo, this.model.path, metadata, this.editor.getValue(), function(err) {
-      console.log('Saved');
-    });
-    return false;
-  },
-
-  initialize: function(options) {
-    
-  },
-
-  initEditor: function() {
-    var that = this;
-    setTimeout(function() {
-      that.editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-          mode: 'markdown',
-          lineNumbers: true,
-          lineWrapping: true,
-          matchBrackets: true,
-          theme: "default"
-      });
-    }, 200);
-  },
-
-  render: function() {
-    var that = this;
-    $(this.el).html(templates.post(this.model));
-
-    this.initEditor();
-
-    return this;
-  }
 });
 
 }).apply(this, window.args);
