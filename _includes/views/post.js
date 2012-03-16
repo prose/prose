0e2505b1b10b6e13bc5d0e49f2b88d6e52dc171c
Michael Aufreiter <ma@zive.at>
2012-03-16 16:06:40 -0400

    Implemented publishing workflow. Publish / Unpublish / Republish.

--- _includes/views/post.js
+++ _includes/views/post.js
@@ -5,18 +5,50 @@ views.Post = Backbone.View.extend({
   id: 'post',
 
   events: {
-    'click .save-post' : 'save',
-    'click .state a': 'publish',
-    'click .meta': 'toggleMeta',
-    'click a.toggle-mode': 'toggleMode'
+    'click .publish': '_publish',
+    'click .unpublish': '_unpublish',
+    'click .meta': '_toggleMeta',
+    'click a.toggle-mode': '_toggleMode',
+    'change input': '_makeDirty'
   },
 
-  publish: function(e) {
-    e.preventDefault();
-    // TODO replace save with publish and pass in commit message options
+  _makeDirty: function() {
+    this.$('.button.publish').removeClass('hidden');
   },
 
-  toggleMode: function(e) {
+  _publish: function(e) {
+    if (this.$('.message').hasClass('hidden')) {
+      this.$('.message').removeClass('hidden'); 
+    } else {
+      this.updatePost(false, this.$('#commit_message').val());
+    }
+    return false;
+  },
+
+  _unpublish: function() {
+    this.updatePost(true, "Unpublish "+ this.model.file);
+    return false;
+  },
+
+  updatePost: function(hidden, message) {
+    var metadata = {
+      title: this.$('#post_title').val(),
+      subtitle: this.$('#post_subtitle').val(),
+      layout: this.$('#post_layout').val(),
+      category: this.$('#post_category').val(),
+      permalink: this.$('#post_permalink').val(),
+      image: this.$('#post_image').val(),
+      hidden: hidden
+    };
+
+    savePost(app.state.user, app.state.repo, app.state.branch, this.model.path, this.model.file, metadata, this.editor.getValue(), message, _.bind(function(err) {
+      this.model.metadata = metadata;
+      this.updatePublishStatus();
+      console.log('saaved');
+    }, this));
+  },
+
+  _toggleMode: function(e) {
     e.preventDefault();
     var m = $(e.currentTarget)
     if (m.hasClass('preview')) {
@@ -31,37 +63,28 @@ views.Post = Backbone.View.extend({
         .html('preview');
     }
   },
-  toggleMeta: function(e) {
+
+  _toggleMeta: function(e) {
     e.preventDefault();
     $('.metadata').toggleClass('open');
   },
 
-  save: function() {
-    var metadata = {
-      title: this.$('#post_title').val(),
-      subtitle: this.$('#post_subtitle').val(),
-      layout: this.$('#post_layout').val(),
-      category: this.$('#post_category').val(),
-      permalink: this.$('#post_permalink').val(),
-      image: this.$('#post_image').val(),
-      hidden: !this.$('#post_published').val()
-    };
 
-    var message = this.$('#commit_message').val();
-    savePost(app.state.user, app.state.repo, app.state.branch, this.model.path, this.model.file, metadata, this.editor.getValue(), message, function(err) {
-      console.log('Saved');
-    });
-    return false;
+  updatePublishStatus: function() {
+    this.$('#publish_status').html(templates.publish_status(this.model));
   },
 
   initEditor: function() {
     var that = this;
     setTimeout(function() {
       that.editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-          mode: 'markdown',
-          lineWrapping: true,
-          matchBrackets: true,
-          theme: "default"
+        mode: 'markdown',
+        lineWrapping: true,
+        matchBrackets: true,
+        theme: "default",
+        onChange: function() {
+          that._makeDirty();
+        }
       });
     }, 200);
   },
@@ -70,6 +93,7 @@ views.Post = Backbone.View.extend({
     var that = this;
     $(this.el).html(templates.post(this.model));
 
+    this.updatePublishStatus();
     this.initEditor();
     return this;
   }
