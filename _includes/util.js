0aab4667ae24d6f22b7d113b535eb81fe1154249
Michael Aufreiter <ma@zive.at>
2012-06-06 12:20:11 -0400

    Allow nested objects.

--- _includes/util.js
+++ _includes/util.js
@@ -1,19 +1,17 @@
 // Putting util functions into the _ namespace, which is handy
 
-
 // Run an array of functions in serial.
 _.serial = function () {
-    (_(arguments).reduceRight(_.wrap, function() {}))();
+  (_(arguments).reduceRight(_.wrap, function() {}))();
 };
 
-
 // Convert a javascript object to its YAML representation
+// IMPORTANT! Not working reliably with nested objects
 _.toYAML = function(data) {
   function serializeValue(val) {
     function serializeArray(a) {
       return a.map(function(elem) { return "- "+ elem}).join("\n");
     }
-
     if (_.isArray(val)) return "\n"+serializeArray(val);
     return _.isNumber(val) || _.isBoolean(val) ? val : "\""+val+"\"";
   }
