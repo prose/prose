a08dd80b61fcf1a36a799965608c2ce4ff11e5ad
Michael Aufreiter <ma@zive.at>
2012-06-13 22:38:13 -0400

    Logout edge cases.

--- _includes/views/header.js
+++ _includes/views/header.js
@@ -10,6 +10,12 @@ views.Header = Backbone.View.extend({
   _logout: function() {
     logout();
     app.instance.render();
+    if ($('#start').length > 0) {
+      app.instance.start();
+    } else {
+      app.instance.mainView.render();
+    }
+    
     return false;
   },
 
