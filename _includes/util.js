8a86b86d86e90c3c77edfba781a83b4aada9c6e6
Michael Aufreiter <ma@zive.at>
2012-06-23 00:02:54 -0400

    Use chunked path for more pleasant navigation.
    Closes #96.

--- _includes/util.js
+++ _includes/util.js
@@ -1,29 +1,69 @@
-// Putting util functions into the _ namespace, which is handy
+// Run an array of functions in serial
+// -------
 
-// Run an array of functions in serial.
 _.serial = function () {
   (_(arguments).reduceRight(_.wrap, function() {}))();
 };
 
+
+// Parent path
+// -------
+
 _.parentPath = function(path) {
   return path.replace(/\/?[a-zA-Z0-9_-]*$/, "");
 };
+
+
+// Topmost path
+// -------
 
 _.topPath = function(path) {
   var match = path.match(/\/?([a-zA-Z0-9_-]*)$/);
   return match[1];
 };
 
-// Check for a valid file name
+
+// Valid filename check
+// -------
+
 _.validFilename = function(filename) {
   return !!filename.match(/^([a-zA-Z0-9_-]|\.)+$/);
   // Disabled for now: the Jekyll post format layout
   // return !!filename.match(/^\d{4}-\d{2}-\d{2}-[a-zA-Z0-9_-]+\.md$/);
 };
 
+
 // Check for a valid post file name
+// -------
+
 _.clip = function(str, length) {
   var res = str.substr(0, length);
   if (length < str.length) res += " ...";
   return res;
 };
+
+
+// Chunked Path
+// -------
+// 
+// _.chunkedPath('path/to/foo')
+// =>
+// [
+//   { url: 'path',        name: 'path' },
+//   { url: 'path/to',     name: 'to' },
+//   { url: 'path/to/foo', name: 'foo' }
+// ]
+
+_.chunkedPath = function(path) {
+  var chunks = path.split('/');
+  return _.map(chunks, function(chunk, index) {
+    var url = [];
+    for (var i=0; i<=index; i++) {
+      url.push(chunks[i]);
+    }
+    return {
+      url: url.join('/'),
+      name: chunk
+    }
+  });
+}
