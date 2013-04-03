fca31b4449df7e6546bbec0355ebf524cd5c344d
mikemorris <michael.patrick.morris@gmail.com>
2013-04-03 10:59:25 -0400

    simplifying test cases by using complex data

--- test/spec/model/model.js
+++ test/spec/model/model.js
@@ -1,112 +1,55 @@
 describe('model.js', function() {
   describe('getFiles', function() {
-    var empty;
-    var file;
-    var files;
-    var directory;
-    var subdirectory;
+    var tree = data.tree;
 
     it('should be a function', function() {
       expect(getFiles).to.be.a('function');
     })
 
     describe('tree', function() {
-      beforeEach(function() {
-        empty = getFiles(data.tree.empty);
-        file = getFiles(data.tree.file);
-        files = getFiles(data.tree.files);
-        directory = getFiles(data.tree.directory);
-        subdirectory = getFiles(data.tree.subdirectory);
-      });
-
-      it('does not return tree if tree is undefined', function() {
+      it('returns tree if it exists', function() {
         expect(getFiles().tree).to.not.exist;
+        expect(getFiles([]).tree).to.exist;
+        expect(getFiles(tree).tree).to.exist;
       });
 
-      it('returns tree, files, and total if tree is NOT undefined', function() {
-        expect(getFiles().files).to.exist;
-        expect(getFiles().total).to.exist;
-
-        expect(empty.files).to.exist;
-        expect(empty.total).to.exist;
-
-        expect(file.files).to.exist;
-        expect(file.total).to.exist;
-
-        expect(files.files).to.exist;
-        expect(files.total).to.exist;
-
-        expect(directory.files).to.exist;
-        expect(directory.total).to.exist;
-
-        expect(subdirectory.files).to.exist;
-        expect(subdirectory.total).to.exist;
+      it('returns same tree as input if it exists', function() {
+        expect(getFiles(tree).tree).to.eql(tree);
       });
 
-      it('returns same tree as input if tree is NOT undefined', function() {
-        expect(empty.tree).to.eql(data.tree.empty);
-        expect(file.tree).to.eql(data.tree.file);
-        expect(files.tree).to.eql(data.tree.files);
-        expect(directory.tree).to.eql(data.tree.directory);
-        expect(subdirectory.tree).to.eql(data.tree.subdirectory);
+      it('returns files and total', function() {
+        expect(getFiles(tree).files).to.exist;
+        expect(getFiles(tree).total).to.exist;
       });
 
-      it('returns correct number of files (not folders) in files array', function() {
-        expect(getFiles().files).to.have.length(0);
-        expect(empty.files).to.have.length(0);
-        expect(file.files).to.have.length(1);
-        expect(files.files).to.have.length(3);
-        expect(directory.files).to.have.length(1);
-        expect(subdirectory.files).to.have.length(1);
+      it('returns correct number of files in files array', function() {
+        expect(getFiles(tree).files).to.have.length(3);
       });
 
-      it('returns correct number of files (not folders) as total', function() {
-        expect(getFiles().total).to.equal(0);
-        expect(empty.total).to.equal(0);
-        expect(file.total).to.equal(1);
-        expect(files.total).to.equal(3);
-        expect(directory.total).to.equal(1);
-        expect(subdirectory.total).to.equal(1);
+      it('returns correct number of files as total', function() {
+        var treeFiles = getFiles(tree);
+        expect(treeFiles.total).to.equal(treeFiles.files.length);
       });
     });
 
     describe('matchesPath', function() {
-      it('returns all files if path is empty string', function() {
-        expect(getFiles(data.tree.empty, '').files).to.have.length(0);
-        expect(getFiles(data.tree.file, '').files).to.have.length(1);
-        expect(getFiles(data.tree.files, '').files).to.have.length(3);
-        expect(getFiles(data.tree.directory, '').files).to.have.length(1);
-        expect(getFiles(data.tree.subdirectory, '').files).to.have.length(1);
+      it('returns only files at root if path is empty string', function() {
+        expect(getFiles(tree, '').files).to.have.length(3);
       });
 
       it('returns only files in path directory', function() {
-        expect(getFiles(data.tree.empty, 'a').files).to.have.length(0);
-        expect(getFiles(data.tree.file, 'a').files).to.have.length(0);
-        expect(getFiles(data.tree.files, 'a').files).to.have.length(0);
-
-        expect(getFiles(data.tree.directory, 'a').files).to.have.length(1);
-        expect(getFiles(data.tree.directory, 'b').files).to.have.length(0);
-
-        expect(getFiles(data.tree.subdirectory, 'a/aa').files).to.have.length(1);
-        expect(getFiles(data.tree.subdirectory, 'b').files).to.have.length(0);
+        expect(getFiles(tree, 'a/aa').files).to.have.length(1);
+        expect(getFiles(tree, 'b').files).to.have.length(0);
       });
     });
 
     describe('matchesSearch', function() {
       it('returns only files and folders matching searchstr', function() {
-        expect(getFiles(data.tree.empty, '', 'a').files).to.have.length(0);
-        expect(getFiles(data.tree.file, '', 'a').files).to.have.length(1);
-        expect(getFiles(data.tree.files, '', 'a').files).to.have.length(1);
-        expect(getFiles(data.tree.directory, '', 'a').files).to.have.length(2);
-        expect(getFiles(data.tree.subdirectory, '', 'a').files).to.have.length(3);
+        expect(getFiles(tree, '', 'a').files).to.have.length(3);
       });
 
-      it('returns only files and folders under path matching searchstr', function() {
-        expect(getFiles(data.tree.empty, 'a', 'a').files).to.have.length(0);
-        expect(getFiles(data.tree.file, 'a', 'a').files).to.have.length(0);
-        expect(getFiles(data.tree.files, 'a', 'a').files).to.have.length(0);
-        expect(getFiles(data.tree.directory, 'a', 'a').files).to.have.length(1);
-        expect(getFiles(data.tree.subdirectory, 'a', 'a').files).to.have.length(2);
+      it('returns only files and folders in path matching searchstr', function() {
+        expect(getFiles(tree, 'a', 'a').files).to.have.length(2);
       });
     });
   });
