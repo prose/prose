cc64e335383568eed0aa3df93fd6c175ef698d73
tristen <tristen@tristen.ca>
2013-07-09 12:09:54 -0400

    Variablizing the repo listings

--- app/views/li/repo.js
+++ app/views/li/repo.js
@@ -4,15 +4,14 @@ var Backbone = require('backbone');
 var templates = require('../../../dist/templates');
 
 module.exports = Backbone.View.extend({
-  template: _.template(templates.li.repo),
-
   tagName: 'li',
 
   className: 'item clearfix',
 
+  template: templates.li.repo,
+
   initialize: function(options) {
     this.model = options.model;
-
     this.listenTo(this.model, 'sync', this.render, this);
   },
 
@@ -20,7 +19,10 @@ module.exports = Backbone.View.extend({
     this.$el.data('navigate', '#' + this.model.get('owner').login + '/' + this.model.get('name'));
     this.$el.data('id', this.model.id);
 
-    this.$el.html(this.template(this.model.attributes));
+    var repo = this.model.attributes;
+    this.$el.empty().append(_.template(this.template, repo, {
+      variable: 'repo'
+    }));
 
     return this;
   }
