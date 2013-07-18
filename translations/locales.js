293a82c684b1fad2dc45cc75dc1cdaf11302d19a
tristen <tristen@tristen.ca>
2013-07-18 17:33:03 -0400

    Dropping the spanish entry as there isnt currently a translator for this

--- translations/locales.js
+++ translations/locales.js
@@ -1,3 +1,3 @@
 // Automatically Generated
 
-module.exports = [{"name":"Chinese","code":"zh"},{"name":"English","code":"en"},{"name":"German","code":"de"},{"name":"Romanian","code":"ro"},{"name":"Spanish","code":"es"},{"name":"Vietnamese","code":"vi"}];
+module.exports = [{"name":"Chinese","code":"zh"},{"name":"English","code":"en"},{"name":"German","code":"de"},{"name":"Romanian","code":"ro"},{"name":"Vietnamese","code":"vi"}];
