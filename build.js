c3683f6d6963e120553aa04857902f9d4eb930b2
Mike Morris <michael.patrick.morris@gmail.com>
2013-07-19 16:01:12 -0400

    switching fs.exists check in build.js to conditional mkdir in Makefile

--- build.js
+++ build.js
@@ -34,10 +34,6 @@ var templates = glob.sync('templates/**/*.html').reduce(function(memo, file) {
   return memo;
 }, {});
 
-if(!fs.exists('dist')) {
-    fs.mkdirSync('dist');
-}
-
 fs.writeFileSync('dist/templates.js', 'module.exports = ' + JSON.stringify(templates) + ';');
 
 // Default language is english. Cache this as a data for speed.
