2ebdfa29adddc94b0d91bb6079fae184994af16d
tristen <tristen@tristen.ca>
2013-08-04 14:14:34 -0400

    Updating languages and accounting for language code instances that containn hyphens

--- translations/update_locales.js
+++ translations/update_locales.js
@@ -1,4 +1,14 @@
-/* Downloads the latest translations from Transifex */
+/*
+ * Downloads the latest translations from Transifex.
+ *
+ * Note: Transifex oddly doesn't allow anonymous downloading
+ * auth is stored in transifex.auth in a json object:
+ *  {
+ *      "user": "username",
+ *      "pass": "password"
+ *  }
+ */
+
 var request = require('request');
 var yaml = require('js-yaml');
 var fs = require('fs');
@@ -8,14 +18,6 @@ var api = 'http://www.transifex.com/api/2/';
 var auth = JSON.parse(fs.readFileSync('./translations/transifex.auth', 'utf8'));
 var project = api + 'project/prose/';
 
-/*
- * Transifex oddly doesn't allow anonymous downloading
- * auth is stored in transifex.auth in a json object:
- *  {
- *      "user": "username",
- *      "pass": "password"
- *  }
- */
 asyncMap(['application'], getResource, function(err, locales) {
     if (err) return console.log(err);
     var locale = yaml.load(fs.readFileSync('./translations/application.yaml', 'utf8'));
@@ -25,9 +27,6 @@ asyncMap(['application'], getResource, function(err, locales) {
     });
 
     for (var i in locale) {
-
-        // Make some expections
-        if (i === 'pt-BR') i = 'pt';
         fs.writeFileSync('./translations/locales/' + i + '.json', JSON.stringify(locale[i], null, 4));
     }
 });
@@ -40,11 +39,10 @@ function getResource(resource, callback) {
         asyncMap(lang, getLanguage(resource), function(err, results) {
             if (err) return callback(err);
             var locale = {};
-
             results.forEach(function(result, i) {
                 var code = lang[i].code;
-                if (code === 'pt-BR') code = 'pt'
-                locale[code] = yaml.load(result)[lang[i].code];
+
+                locale[code] = result;
             });
 
             callback(null, locale);
@@ -56,10 +54,13 @@ function getResource(resource, callback) {
 
 function getLanguage(resource) {
     return function(lang, callback) {
-        request.get(resource + 'translation/' + lang.code, { auth : auth },
+        var code = lang.code.replace(/-/g, '_');
+
+        request.get(resource + 'translation/' + code, { auth : auth },
             function(err, resp, body) {
             if (err) return callback(err);
-            callback(null, JSON.parse(body).content);
+
+            callback(null, yaml.load(JSON.parse(body).content)[code]);
         });
     };
 }
@@ -69,9 +70,10 @@ function getLanguages(resource, callback) {
         function(err, resp, body) {
         if (err) return callback(err);
         callback(null, JSON.parse(body).available_languages.map(function(d) {
+            var code = d.code.replace(/_/g, '-');
             return {
               name: d.name,
-              code: d.code.replace(/_/g, '-')
+              code: code
             }
         }).filter(function(d) {
             return d !== 'en';
