610b7b60e8878cae089ec99858a17ce618cb8377
tristen <tristen@tristen.ca>
2013-09-09 11:02:55 -0400

    Updating translations

--- translations/locales.js
+++ translations/locales.js
@@ -1,3 +1,3 @@
 // Automatically Generated
 
-module.exports = [{"name":"Chinese","code":"zh"},{"name":"English","code":"en"},{"name":"French","code":"fr"},{"name":"German","code":"de"},{"name":"Portuguese (Brazil)","code":"pt-BR"},{"name":"Romanian","code":"ro"},{"name":"Russian","code":"ru"},{"name":"Spanish","code":"es"},{"name":"Swedish","code":"sv"},{"name":"Vietnamese","code":"vi"}];
+module.exports = [{"name":"Chinese","code":"zh"},{"name":"Dutch","code":"nl"},{"name":"English","code":"en"},{"name":"French","code":"fr"},{"name":"German","code":"de"},{"name":"Portuguese (Brazil)","code":"pt-BR"},{"name":"Romanian","code":"ro"},{"name":"Russian","code":"ru"},{"name":"Spanish","code":"es"},{"name":"Swedish","code":"sv"},{"name":"Vietnamese","code":"vi"}];
