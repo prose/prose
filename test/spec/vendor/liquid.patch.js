a8e376bd2a75523e74babadd146eac7198fc5bb1
Zach Starkebaum <zach@z-top.local>
2014-04-22 17:55:36 -0600

    Liquid test verbiage update

--- test/spec/vendor/liquid.patch.js
+++ test/spec/vendor/liquid.patch.js
@@ -21,23 +21,23 @@ describe('liquid.patch', function() {
       expect(render).to.throw();
     })
 
-    it('should evaluate a variable', function() {
+    it('should evaluate an argument', function() {
       var render = function () {
         return Liquid.parse("{% include test.html var1='test1' %}").render();
       }
       expect(render()).to.equal("test1\n\n\n");
     })
 
-    it('should evaluate a few variables', function() {
+    it('should evaluate a few arguments', function() {
       var render = function () {
         return Liquid.parse("{% include test.html var1='test1' var2='test2' var3='test3' %}").render();
       }
       expect(render()).to.equal("test1\ntest2\ntest3\n");
     })
 
-    it('should interpret variables', function() {
+    it('should interpret variables in arguments', function() {
       var render = function () {
-        return Liquid.parse("{% include test.html var1=shoe var2=shoe var3=shoe %}").render({ shoe: 'box'});
+        return Liquid.parse("{% include test.html var1=shoe var2= shoe var3=shoe %}").render({ shoe: 'box'});
       }
       expect(render()).to.equal("box\nbox\nbox\n");
     })
