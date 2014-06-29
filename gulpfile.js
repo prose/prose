12cdee3c79b25983cbec01a4d7feda1a9d817955
Pablo Vallejo <pabloninety@gmail.com>
2014-06-29 01:02:05 -0500

    Gulp Build: Fix indentation.

--- gulpfile.js
+++ gulpfile.js
@@ -12,27 +12,27 @@ var clean = require('gulp-clean');
 
 // Scripts paths.
 var paths = {
-	vendorScripts: [
-		'vendor/codemirror/codemirror.js',
-		'vendor/codemirror/overlay.js',
-		'vendor/codemirror/htmlmixed.js',
-		'vendor/codemirror/clike.js',
-		'vendor/codemirror/yaml.js',
-		'vendor/codemirror/ruby.js',
-		'vendor/codemirror/markdown.js',
-		'vendor/codemirror/xml.js',
-		'vendor/codemirror/javascript.js',
-		'vendor/codemirror/css.js',
-		'vendor/codemirror/gfm.js',
-		'vendor/liquid.js'
-	]
+  vendorScripts: [
+    'vendor/codemirror/codemirror.js',
+    'vendor/codemirror/overlay.js',
+    'vendor/codemirror/htmlmixed.js',
+    'vendor/codemirror/clike.js',
+    'vendor/codemirror/yaml.js',
+    'vendor/codemirror/ruby.js',
+    'vendor/codemirror/markdown.js',
+    'vendor/codemirror/xml.js',
+    'vendor/codemirror/javascript.js',
+    'vendor/codemirror/css.js',
+    'vendor/codemirror/gfm.js',
+    'vendor/liquid.js'
+  ]
 };
 
 
 // Removes `build` folder.
 gulp.task('clean', function () {
 
-	// Use `gulp-clean` to remove folder.
+  // Use `gulp-clean` to remove folder.
   return gulp.src('dist', {read: false})
     .pipe(clean());
 });
@@ -42,21 +42,21 @@ gulp.task('clean', function () {
 // To run this task we have to have a `transifex.auth`
 // file inside `translations` folder.
 // Example file contents:
-// 		
-// 	{
-// 		"user": "",
-// 		"pass": ""
-// 	}
+//    
+//  {
+//    "user": "",
+//    "pass": ""
+//  }
 // 
 // An account can be created at https://www.transifex.com/
 // 
 gulp.task('translations', function () {
   return gulp.src('')
     .pipe(
-    	shell([
-    		'mkdir -p dist',
-    		'node translations/update_locales',
-			])
+      shell([
+        'mkdir -p dist',
+        'node translations/update_locales',
+      ])
     )
 });
 
@@ -68,9 +68,9 @@ gulp.task('translations', function () {
 gulp.task('templates', function () {
   return gulp.src('')
     .pipe(
-    	shell([
-    		'mkdir -p dist && node build'
-			])
+      shell([
+        'mkdir -p dist && node build'
+      ])
     )
 });
 
@@ -80,23 +80,23 @@ gulp.task('templates', function () {
 gulp.task('oauth', function () {
   return gulp.src('')
     .pipe(
-    	shell([
-    		'mkdir -p dist',
-  			'curl "https://raw.githubusercontent.com/prose/prose/gh-pages/oauth.json" > oauth.json'
-			])
+      shell([
+        'mkdir -p dist',
+        'curl "https://raw.githubusercontent.com/prose/prose/gh-pages/oauth.json" > oauth.json'
+      ])
     )
 });
 
 
 // Built tests.
 gulp.task('tests', function() {
-	return gulp.src('')
+  return gulp.src('')
     .pipe(
-		shell([
-			'browserify -d test/index.js -o test/lib/index.js',
-			'browserify test/lib/polyfill-require.js -o test/lib/polyfill.js'
-		])	
-	)
+    shell([
+      'browserify -d test/index.js -o test/lib/index.js',
+      'browserify test/lib/polyfill-require.js -o test/lib/polyfill.js'
+    ])  
+  )
 });
 
 
@@ -108,9 +108,9 @@ gulp.task('scripts', ['templates', 'oauth'], function() {
     .pipe(concat('prose.js'))
     .pipe(gulp.dest('dist/'))
     .pipe(
-    	shell([
-				'browserify -d app/boot.js >> dist/prose.js'
-			])
+      shell([
+        'browserify -d app/boot.js >> dist/prose.js'
+      ])
     )
     
 });
@@ -119,10 +119,10 @@ gulp.task('scripts', ['templates', 'oauth'], function() {
 // Compress `prose.js`.
 gulp.task('uglify', ['scripts'], function() {
 
-	return gulp.src('dist/prose.js')
-		.pipe(rename('prose.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'))
+  return gulp.src('dist/prose.js')
+    .pipe(rename('prose.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
 });
 
 
