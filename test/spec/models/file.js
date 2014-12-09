d9ae8bca4c22e383f7f8527249e1f99035a1a49a
Anand Thakker <vestibule@anandthakker.net>
2014-12-09 12:00:25 +0100

    tests: minor edits for conciseness

--- test/spec/models/file.js
+++ test/spec/models/file.js
@@ -2,34 +2,45 @@
 var File = require('../../../app/models/file');
 
 var mocks = require('../mocks'),
-    response = require('../../fixtures/get-file-response.json');
+    filedata = require('../../fixtures/get-file-response.json');
 
 
 describe('file model', function() {
-  var file;
+  var mock;
   var server, callbacks, fileContents;
   
   before(function () { server = sinon.fakeServer.create(); });
   after(function () { server.restore(); });
-  
   beforeEach(function() {
-    file = mocks().file();
+    mock = mocks();
     callbacks = mocks.stubs(['success', 'error', 'complete']);
-    server.respondWith('GET', file.url(), JSON.stringify(response));
-    file.set('content_url', 'https://api.gihub.com/repos/blah/blahblah/git/blahblahblah')
   })
   
+  /*
+  Create a mock File model and set up the fake server to respond to a fetch().
+  Returns the mock file.
+  */
+  var filecount = 0;
+  function  mockFile(content, data) {
+    var file = mock.file();
+    server.respondWith('GET', file.url(), JSON.stringify(data));
+    file.set('content_url', 'https://api.gihub.com/repos/blah/blahblah/git/'+filecount++);
+    server.respondWith('GET', file.get('content_url'), content);
+    return file;
+  }
+
+
 
   it('fetches data from github api and content directly content URL', function() {
     
-    var content = 'content from server';
+    var content = 'content from server',
+        file = mockFile(content, filedata);
     
     expect(file.get('content')).to.not.equal(content);
-    expect(file.get('sha')).to.not.equal(response.sha);
+    expect(file.get('sha')).to.not.equal(filedata.sha);
     
     file.fetch(callbacks);
     
-    server.respondWith('GET', file.get('content_url'), content);
     server.respond();
     
     expect(callbacks.success).to.have.been.calledOnce;
@@ -37,16 +48,16 @@ describe('file model', function() {
     expect(server.requests.length).to.equal(2);
 
     expect(file.get('content')).to.equal(content);
-    expect(file.get('sha')).to.equal(response.sha);
+    expect(file.get('sha')).to.equal(filedata.sha);
   })
   
   
   it('parses YAML frontmatter when present', function() {
     var content = 'my content',
-        yaml = '---\nlayout: post\npublished: true\n---\n';
+        yaml = '---\nlayout: post\npublished: true\n---\n',
+        file = mockFile(yaml + content, filedata);
      
     file.fetch(callbacks);
-    server.respondWith('GET', file.get('content_url'), yaml + content);
     server.respond();
     
     expect(callbacks.success).to.have.been.calledOnce;
@@ -57,31 +68,27 @@ describe('file model', function() {
     });
   })
   
-  it('appends a single newline if not already present', function() {
-    var content = 'my content',
-        yaml = '---\nlayout: post\npublished: true\n---\n';
-    
-    file.fetch(callbacks);
-    server.respondWith('GET', file.get('content_url'), yaml + content);
-    server.respond();
-    
-    expect(callbacks.success).to.have.been.calledOnce;
-    
-    expect(file.get('content')).to.equal(content + '\n');
-  });
-  
-  it('trims all whitespace *except* final newline from end of content', function() {
+  describe('trailing whitespace', function() {
     var content = 'my content',
         extraspace = '\n\t  \t\t\n   \n\t\t \n',
         yaml = '---\nlayout: post\npublished: true\n---\n';
+        
+    it('appends a single newline if not already present', function() {
+      var file = mockFile(yaml + content, filedata);
+      file.fetch(callbacks);
+      server.respond();
+      expect(callbacks.success).to.have.been.calledOnce;
+      expect(file.get('content')).to.equal(content + '\n');
+    });
     
-    file.fetch(callbacks);
-    server.respondWith('GET', file.get('content_url'), yaml + content + extraspace);
-    server.respond();
-    
-    expect(callbacks.success).to.have.been.calledOnce;
-    
-    expect(file.get('content')).to.equal(content + '\n');
+    it('trims all whitespace *except* final newline from end of content', function() {
+      var file = mockFile(yaml + content + extraspace, filedata)
+      file.fetch(callbacks);
+      server.respond();
+      expect(callbacks.success).to.have.been.calledOnce;
+      expect(file.get('content')).to.equal(content + '\n');
+    })
   })
+
 
 });
